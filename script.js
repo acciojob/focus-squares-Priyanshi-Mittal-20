60052e313d5424fcd4a53953f0baa0bfd3d4789d
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-02-15 14:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let squares=document.getElementsByClassName("square")
 for(let i of squares)
 	{
-		i.addEventListener("click",changeColor)
+		i.addEventListener("mouseover",changeColor)
+		i.addEventListener("mouseout",resetColor)
 }
 function changeColor(e)
 {
@@ -11,7 +12,14 @@ function changeColor(e)
 		{
 			if(j != ele)
 			{
-				j.style.backgroundColor="rgb(111,78,55)";
+				j.style.background-color="rgb(111,78,55)";
 }
+}
+}
+function resetColor(e)
+{
+	for(let k of squares)
+		{
+			k.style.background-color="rgb(230,230,250)"
 }
 }
