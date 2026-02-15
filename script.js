27da95eb9c18b180fbda9781f741f78bc409e297
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-02-15 14:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let squares=document.getElementsByClassName("square")
+for(let i of squares)
+	{
+		i.addEventListener("click",changeColor)
+}
+function changeColor(e)
+{
+	let ele=e.target
+	for(let j of squares)
+		{
+			if(j!=ele)
+			{
+				j.style.backgroundColor="coffee"
+}
+}
+}
